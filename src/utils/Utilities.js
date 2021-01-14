134ea45d5b1042e63f38c446a6710a851dfa4545
antonin.juquel <antonin.juquel@woody-technologies.com>
2021-01-14 11:32:27 +0100

    More efficient code

--- src/utils/Utilities.js
+++ src/utils/Utilities.js
@@ -1,22 +1,29 @@
-export function deserializeFile(directoryId, file, isDir) {
-    const { type, path } = isDir ? file.AVStruct : { type: file.AVType, path: [...directoryId, "/"].join("/") + file.Name }
+function pathToList(path) {
+    return unescape(path).replaceAll("\\", "/").split("/").filter(dir => dir !== "")
+}
+export function deserializeFile(file, isDir) {
     return {
-        id: unescape(path).replaceAll("\\", "/").split("/").filter(dir => dir !== ""),
         name: unescape(file.Name),
         isDir,
-        modDate: file.Date,
-        childrenCount: isDir && (file.count === undefined ? 0 : file.count),
-        type,
-        icon: { isDir, type },
+        modDate: file.Date
     }
 }
 export function deserializeFiles(directoryId, jsonFiles) {
     let files = []
     jsonFiles.SubFolders && jsonFiles.SubFolders.forEach(folder => {
-        files.push(deserializeFile(directoryId, folder, true))
+        let icon = "folder"
+        if(folder.count)
+            icon += ",video"
+        files.push({
+            ...deserializeFile(folder, true),
+            ...{ id: pathToList(folder.AVStruct.path), type: folder.AVStruct.type, childrenCount: folder.count, icon }
+        })
     })
     jsonFiles.Files && jsonFiles.Files.forEach(file => {
-        files.push(deserializeFile(directoryId, file, false))
+        files.push({
+            ...deserializeFile(file, false),
+            ...{ id: pathToList([...directoryId, "/"].join("/") + file.Name), type: file.AVType }
+        })
     })
     return files
 }
