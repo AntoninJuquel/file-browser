81d02680ad8fa184d6fa1f1f51256fd273f44dee
Antonin Juquel <antonin.juquel@woody-technologies.com>
2021-01-22 14:50:07 +0100

    Update Browser.js

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useMemo } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FullFileBrowser, ChonkyActions } from "chonky";
 import { Rnd } from "react-rnd";
 
